7a5ce4e1db51e7e04bdc6ebf71df35042c13bf67
Elodie <e.dehache@gmail.com>
2020-10-01 13:22:04 +0200

    Set up Eureka server

--- src/main/java/com/example/cloudprojectregistry/CloudProjectRegistryApplication.java
+++ src/main/java/com/example/cloudprojectregistry/CloudProjectRegistryApplication.java
@@ -2,8 +2,10 @@ package com.example.cloudprojectregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CloudProjectRegistryApplication {
 
 	public static void main(String[] args) {
