900739821fa5fa5a1aa3d033f6b7ec95f00ccdfd
Elodie <e.dehache@gmail.com>
2020-10-01 12:56:36 +0200

    'Init'

---
+++ src/main/java/com/example/cloudprojectregistry/CloudProjectRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudprojectregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudProjectRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudProjectRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudprojectregistry/CloudProjectRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudprojectregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProjectRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
